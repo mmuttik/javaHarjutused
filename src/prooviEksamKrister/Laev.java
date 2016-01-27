3fa734317bf22a2bf02c0181b56489da9136ef90
mmuttik <marttimuttik@gmail.com>
2016-01-27 23:24:52 +0200

    Misc. changes

--- src/prooviEksamKrister/Laev.java
+++ src/prooviEksamKrister/Laev.java
@@ -8,44 +8,37 @@ import java.util.Random;
  */
 public class Laev {
 
-        private String name;
-        private ArrayList<String> passengers = new ArrayList<>();
-        private Random randomGenerator;
+    private String name;
+    private ArrayList<String> passengers = new ArrayList<>();
+    private Random randomGenerator;
 
-        public Laev(String shipName)
-        {
-            this.name = shipName;
-            randomGenerator = new Random();
-        }
+    public Laev(String shipName) {
+        this.name = shipName;
+        randomGenerator = new Random();
+    }
 
-        public String misNimi()
-        {
-            return this.name;
-        }
+    public String misNimi() {
+        return this.name;
+    }
 
-        public void lisaReisija(String passengerName)
-        {
-            this.passengers.add(passengerName);
-        }
+    public void lisaReisija(String passengerName) {
+        this.passengers.add(passengerName);
+    }
 
-        public void eemaldaReisija(String passengerName)
-        {
-            this.passengers.remove(passengerName);
-        }
+    public void eemaldaReisija(String passengerName) {
+        this.passengers.remove(passengerName);
+    }
 
-        public String votaReisijad()
-        {
-            return this.passengers.toString();
-        }
+    public String votaReisijad() {
+        return this.passengers.toString();
+    }
 
-        public int reisijateArv()
-        {
-            return this.passengers.size();
-        }
+    public int reisijateArv() {
+        return this.passengers.size();
+    }
 
-        public void kalaHammustasKummipaatiAugu()
-        {
-            int deadPassengerIndex = randomGenerator.nextInt(this.passengers.size());
-            this.passengers.remove(deadPassengerIndex);
-        }
+    public void kalaHammustasKummipaatiAugu() {
+        int deadPassengerIndex = randomGenerator.nextInt(this.passengers.size());
+        this.passengers.remove(deadPassengerIndex);
+    }
 }
